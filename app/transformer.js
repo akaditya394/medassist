61c31a8e9fc761917df2d35227a262e6d9a8a9ea
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-03-14 16:57:25 +0530

    scss in app not working

---
+++ app/transformer.js
@@ -0,0 +1,26 @@
+const upstreamTransformer = require("metro-react-native-babel-transformer");
+const sassTransformer = require("react-native-sass-transformer");
+
+const theme = (process.cwd() + "/src/styles/Global.scss").replace(/\\/g, "/");
+
+module.exports.transform = function ({ src, filename, options }) {
+    if (filename.endsWith(".scss") || filename.endsWith(".sass")) {
+        var opts = Object.assign(options, {
+            sassOptions: {
+                functions: {
+                    "rem($px)": px => {
+                        px.setValue(px.getValue() / 16);
+                        px.setUnit("rem");
+                        return px;
+                    }
+                }
+            }
+        });
+
+        src = `@import "${theme}"; \n\n ` + src;
+
+        return sassTransformer.transform({ src, filename, options: opts });
+    } else {
+        return upstreamTransformer.transform({ src, filename, options });
+    }
+};
