13794d52a1d9e97063de77c9a364ab5356454ce4
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-03-15 10:29:21 +0530

    added navigation, resources and utils

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import styles from './App.scss';
+import LoginScreen from './screens/login';
+import SignUpScreen from './screens/signup';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View className={styles.container}>
-      <Text className={styles.text}>This is app using scss to create styling the components</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
---
+++ app/src/screens/login/index.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react"
+import { Text, View } from 'react-native'
+
+import Notice from "../../components/notice"
+import Input from "../../components/input"
+
+import styles from "./styles.module.scss"
+
+const form = {
+    id: "login",
+    inputs: [
+        {
+            id: "email",
+            type: "email",
+            label: "E-Mail Address",
+            required: true,
+            value: "",
+        },
+        {
+            id: "password",
+            type: "password",
+            label: "Password",
+            required: true,
+            value: "",
+        },
+    ],
+    submitButton: {
+        type: "submit",
+        label: "Login",
+    },
+    button: {
+        type: "button",
+        label: "Forgot password ?",
+    },
+}
+
+const LoginScreen = () => {
+    const RESET_NOTICE = { type: "", message: "" }
+    const [notice, setNotice] = useState(RESET_NOTICE)
+
+    const values = {}
+    // form.inputs.forEach((input) => (values[input.id] = input.value))
+    const [formData, setFormData] = useState(values)
+
+    const handleInputChange = (id: string, value: string) => {
+        setFormData({ ...formData, [id]: value })
+    }
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault()
+        // a http post request to login
+    }
+
+    const handlePasswordReset = (e: any) => {
+        e.preventDefault()
+    }
+
+    return (
+        <View className={styles.loginForm}>
+            <Text className={styles.pageHeading}>Login</Text>
+        </View>
+    )
+}
+
+export default LoginScreen
