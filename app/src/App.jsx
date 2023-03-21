c69b6ddc76ed49db5dfc964f830e63ab82c0feeb
Aditya <akaditya394@gmail.com>
2023-03-21 19:01:49 +0530

    react table removed

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,34 +1,68 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import LoginScreen from './screens/login'
-import SignUpScreen from './screens/signup'
-import AllPrescriptionsScreen from './screens/allPrescriptions'
-import ForgotPasswordScreen from './screens/forgotPassword'
-import AllResultsScreen from './screens/allResults'
-import OnboardingScreen from './screens/onboarding'
-import SettingsScreen from './screens/settings'
-import ResetPasswordScreen from './screens/resetPassword'
-import UploadScreen from './screens/upload'
-import ResultScreen from './screens/result'
+import LoginScreen from "./screens/login";
+import SignUpScreen from "./screens/signup";
+import AllPrescriptionsScreen from "./screens/allPrescriptions";
+import ForgotPasswordScreen from "./screens/forgotPassword";
+import AllResultsScreen from "./screens/allResults";
+import OnboardingScreen from "./screens/onboarding";
+import SettingsScreen from "./screens/settings";
+import ResetPasswordScreen from "./screens/resetPassword";
+import UploadScreen from "./screens/upload";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Onboarding'>
-        <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="AllPrescriptions" component={AllPrescriptionsScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="AllResults" component={AllResultsScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Settings" component={SettingsScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Upload" component={UploadScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Result" component={ResultScreen} options={{ headerShown: false }} />
+      <Stack.Navigator initialRouteName="Onboarding">
+        <Stack.Screen
+          name="Onboarding"
+          component={OnboardingScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ForgotPassword"
+          component={ForgotPasswordScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="AllPrescriptions"
+          component={AllPrescriptionsScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="AllResults"
+          component={AllResultsScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ResetPassword"
+          component={ResetPasswordScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Upload"
+          component={UploadScreen}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
--- app/src/screens/onboarding/index.jsx
+++ app/src/screens/onboarding/index.jsx
@@ -1,46 +1,48 @@
-import { StatusBar } from 'expo-status-bar'
+import { StatusBar } from "expo-status-bar";
 
 import {
-    StyledContainer,
-    InnerContainer,
-    PageTitle,
-    Assist,
-    StyledButton,
-    ButtonText,
-    BottomContainer,
-    SubTitle,
-    StyledList,
-    StyledListItem
-} from './styles'
+  StyledContainer,
+  InnerContainer,
+  PageTitle,
+  Assist,
+  StyledButton,
+  ButtonText,
+  BottomContainer,
+  SubTitle,
+  StyledList,
+  StyledListItem,
+} from "./styles";
 
 const data = [
-    { id: 1, text: 'Item 1' },
-    { id: 2, text: 'Item 2' },
-    { id: 3, text: 'Item 3' },
-]
+  { id: 1, text: "Item 1" },
+  { id: 2, text: "Item 2" },
+  { id: 3, text: "Item 3" },
+];
 
 const OnboardingScreen = ({ navigation }) => {
-    return (
-        <StyledContainer>
-            <StatusBar style='dark' />
-            <InnerContainer>
-                <PageTitle>
-                    med<Assist>assist</Assist>
-                </PageTitle>
-                <SubTitle>A Simple way to help control your health</SubTitle>
-                <StyledList
-                    data={data}
-                    renderItem={({ item }) => <StyledListItem>{`\u2022 ${item.text}`}</StyledListItem>}
-                    keyExtractor={(item) => item.id.toString()}
-                />
-                <BottomContainer>
-                    <StyledButton onPress={() => navigation.navigate('Result')}>
-                        <ButtonText>Get Started</ButtonText>
-                    </StyledButton>
-                </BottomContainer>
-            </InnerContainer>
-        </StyledContainer>
-    )
-}
+  return (
+    <StyledContainer>
+      <StatusBar style="dark" />
+      <InnerContainer>
+        <PageTitle>
+          med<Assist>assist</Assist>
+        </PageTitle>
+        <SubTitle>A Simple way to help control your health</SubTitle>
+        <StyledList
+          data={data}
+          renderItem={({ item }) => (
+            <StyledListItem>{`\u2022 ${item.text}`}</StyledListItem>
+          )}
+          keyExtractor={(item) => item.id.toString()}
+        />
+        <BottomContainer>
+          <StyledButton onPress={() => navigation.navigate("Upload")}>
+            <ButtonText>Get Started</ButtonText>
+          </StyledButton>
+        </BottomContainer>
+      </InnerContainer>
+    </StyledContainer>
+  );
+};
 
-export default OnboardingScreen
+export default OnboardingScreen;
