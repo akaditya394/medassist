4153a2191399213291959c9d53515bbb6e0a8085
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-04-15 00:23:57 +0530

    added verified and unverified result

---
+++ web/components/verifiedResult/index.js
@@ -0,0 +1,24 @@
+import styles from "./styles.module.scss"
+import TestPrescription from "../../images/test/prescription.png"
+import SideEffectsTable from "../sideEffectsTable"
+import Notice from "../notice"
+
+const VerifiedResult = () => {
+    return (
+        <>
+            <Notice>
+                <h3>Prescription verified</h3>
+                <p>Your upload prescription has been verified by a trained medical professional.</p>
+            </Notice>
+            <h1 className="pageHeading">Your verified Prescription</h1>
+            <div className={styles.prescription}>
+                <img src={TestPrescription} alt="Prescription" />
+            </div>
+            <div className={styles.table}>
+                <SideEffectsTable />
+            </div>
+        </>
+    )
+}
+
+export default VerifiedResult
---
+++ web/pages/unverfiedResult.js
@@ -0,0 +1,7 @@
+import UnverifiedResult from "../components/unverifiedResult"
+
+const UnverifiedResultPage = () => {
+    return <UnverifiedResult />
+}
+
+export default UnverifiedResultPage
