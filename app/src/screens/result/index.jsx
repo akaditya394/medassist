bf03964a4ca56b666c9c38a6b048655693a18809
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-05-14 02:05:05 +0530

    table bugs in app fixed

--- app/src/screens/result/index.jsx
+++ app/src/screens/result/index.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { View, Text } from 'react-native'
+import { DataTable } from 'react-native-paper'
 
 import {
     StyledContainer,
@@ -17,18 +20,40 @@ import {
 } from './styles'
 
 import SettingsImage from '../../images/icons/settings.svg'
-import { Table, Row, Rows } from 'react-native-table-component'
 import { Colors } from '../../shared/variables'
 
 const ResultScreen = ({ navigation }) => {
-    const tableHead = ['Drug name', 'Symptoms']
-    const tableData = [
-        ['Microcef CV 200 mg', 'Throat infections'],
-        ['Ventryl D', 'Sore throat'],
-        ['Pantotav DSR', 'Acidity'],
-        ['BENZ Pearls', 'Dry cough'],
-        ['Montak LC', 'Runny nose, watery eyes, sneezing']
-    ]
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        setData({
+            "_id": "645fa16ef5027e9d7f606bb6",
+            "name": "Vansh pres2",
+            "isVerified": false,
+            "image": "https://res.cloudinary.com/ddr0jey9k/image/upload/v1683988845/prescriptions/1683988829935.jpg",
+            "drugs": [
+                "STALOPAM",
+                "STALOPAM",
+                "STALOPAM",
+                "STALOPAM"
+            ],
+            "alternatives": [],
+            "suggestions": [],
+            "sideEffects": [
+                "Increased appetite",
+                "Increased appetite",
+                "Increased appetite",
+                "Increased appetite"
+            ],
+            "user": "645f8be38438b15c858a618b",
+            "doctor": [
+                "645fce8b82b88074925c7ac7"
+            ],
+            "createdAt": "2023-05-13T14:40:46.208Z",
+            "updatedAt": "2023-05-13T18:15:33.502Z",
+            "__v": 0
+        })
+    }, [])
 
     return (
         <StyledContainer>
@@ -49,12 +74,26 @@ const ResultScreen = ({ navigation }) => {
                         <PrescriptionImage resizeMode="cover" source={require('../../images/test/prescription.jpg')} />
                     </SelectImage>
                     <TableContainer>
-                        <Table borderStyle={{ borderWidth: 1, borderColor: `${Colors.primary}` }}>
-                            <Row data={tableHead} style={{
+                        <DataTable>
+                            <DataTable.Header style={{
                                 height: 50, backgroundColor: `${Colors.tertiary}`
-                            }} textStyle={{ margin: 6, fontWeight: 'bold' }} />
-                            <Rows data={tableData} textStyle={{ margin: 6 }} />
-                        </Table>
+                            }}>
+                                <DataTable.Title>Drug name</DataTable.Title>
+                                <DataTable.Title>Side Effects</DataTable.Title>
+                            </DataTable.Header>
+                            {data?.drugs?.map((drug, index) => {
+                                return (
+                                    <DataTable.Row key={index}>
+                                        <DataTable.Cell>{drug}</DataTable.Cell>
+                                        <DataTable.Cell>
+                                            <View style={{ width: 100, flexShrink: 1 }}>
+                                                <Text style={{ textAlign: 'left' }}>{data?.sideEffects?.[index]}</Text>
+                                            </View>
+                                        </DataTable.Cell>
+                                    </DataTable.Row>
+                                )
+                            })}
+                        </DataTable>
                     </TableContainer>
                 </ScrollableContainer>
                 <BottomContainer>
