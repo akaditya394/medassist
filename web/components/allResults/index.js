1cfe0df138e1879c5361ab1deeebc838db6caca3
vanshagarwal18 <vanshagarwal1211@gmail.com>
2023-05-13 23:15:26 +0530

    User side done

--- web/components/allResults/index.js
+++ web/components/allResults/index.js
@@ -9,6 +9,7 @@ import styles from "./styles.module.scss";
 import VerifiedIcon from "../../images/icons/verified.svg";
 import UnverifiedIcon from "../../images/icons/hourglass.svg";
 import axios from "axios";
+import LoaderDarkBig from "../loaderDarkBig";
 
 // const data = [
 //   { id: 1, name: "my prescription", verified: true },
@@ -22,12 +23,14 @@ const AllResultsPage = () => {
   const [notice, setNotice] = useState(RESET_NOTICE);
   const router = useRouter();
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const appState = useContext(StateContext);
 
   useEffect(() => {
     async function prescriptions() {
       try {
         const token = appState.person.token;
+        setIsLoading(true);
         const res = await axios.get("/prescription/allPrescriptions", {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -36,20 +39,16 @@ const AllResultsPage = () => {
 
         switch (res.data.type) {
           case "success":
+            setIsLoading(false);
             setData(res.data.prescriptions);
             break;
           case "error":
+            setIsLoading(false);
             console.log(res);
             break;
         }
-        // console.log(res, "ResPres");
-        // setData([
-        //   { id: 1, name: "my prescription", verified: true },
-        //   { id: 2, name: "test prescription", verified: false },
-        //   { id: 3, name: "Item 2", verified: false },
-        //   { id: 4, name: "Item 3", verified: true },
-        // ]);
       } catch (err) {
+        setIsLoading(false);
         console.log(err);
       }
     }
@@ -64,57 +63,68 @@ const AllResultsPage = () => {
   return (
     <>
       <h1 className="pageHeading">All your results</h1>
-      <div className={styles.gridContainer}>
-        {data.map((data, key) => {
-          return (
-            <NextLink
-              href={
-                data.isVerified
-                  ? {
-                      pathname: `/verifiedResult`,
-                      query: { id: `${data._id}` },
-                    }
-                  : {
-                      pathname: `/unverifiedResult`,
-                      query: { id: `${data._id}` },
-                    }
-              }
-              key={key}
-            >
-              <div className={styles.box}>
-                <div className={styles.name}>{data.name}</div>
-                {data.isVerified ? (
-                  <div className={styles.verified}>
-                    <img src={VerifiedIcon} alt="Verified Icon" />
+      {!isLoading ? (
+        <>
+          <div className={styles.gridContainer}>
+            {data.map((data, key) => {
+              return (
+                <NextLink
+                  href={
+                    data.isVerified
+                      ? {
+                          pathname: `/verifiedResult`,
+                          query: { id: `${data._id}` },
+                        }
+                      : {
+                          pathname: `/unverifiedResult`,
+                          query: { id: `${data._id}` },
+                        }
+                  }
+                  key={key}
+                >
+                  <div className={styles.box}>
+                    <div className={styles.name}>{data.name}</div>
+                    {data.isVerified ? (
+                      <div className={styles.verified}>
+                        <img src={VerifiedIcon} alt="Verified Icon" />
+                      </div>
+                    ) : (
+                      <div className={styles.verified}>
+                        <img src={UnverifiedIcon} alt="Unverified Icon" />
+                      </div>
+                    )}
                   </div>
-                ) : (
-                  <div className={styles.verified}>
-                    <img src={UnverifiedIcon} alt="Unverified Icon" />
-                  </div>
-                )}
-              </div>
-            </NextLink>
-          );
-        })}
-      </div>
-      {data.length >= 5 ? (
-        <Notice style={{ marginTop: "2rem" }}>
-          <h3>Your free tier has been expired</h3>
-          <p>To continue using medassist, upgrade your plan</p>
-          <div className={styles.buttonContainer} onClick={handleUpgradePlan}>
-            <button className={styles.button}>Upgrade Plan</button>
+                </NextLink>
+              );
+            })}
           </div>
-        </Notice>
+          {data.length >= 5 ? (
+            <Notice style={{ marginTop: "2rem" }}>
+              <h3>Your free tier has been expired</h3>
+              <p>To continue using medassist, upgrade your plan</p>
+              <div
+                className={styles.buttonContainer}
+                onClick={handleUpgradePlan}
+              >
+                <button className={styles.button}>Upgrade Plan</button>
+              </div>
+            </Notice>
+          ) : (
+            <div className={styles.buttonContainer}>
+              <button
+                className={styles.button}
+                onClick={() => {
+                  router.push("/prescriptionUpload");
+                }}
+              >
+                Upload Prescription
+              </button>
+            </div>
+          )}
+        </>
       ) : (
-        <div className={styles.buttonContainer}>
-          <button
-            className={styles.button}
-            onClick={() => {
-              router.push("/prescriptionUpload");
-            }}
-          >
-            Upload Prescription
-          </button>
+        <div className={styles.loader}>
+          <LoaderDarkBig />
         </div>
       )}
     </>
